018459bb371e834f0a8e7555a1ff639396b7ebec
Thomas Kildahl <69842791+ThomasK97@users.noreply.github.com>
2021-01-08 11:57:55 +0100

    added code

---
+++ js/script.js
@@ -0,0 +1,44 @@
+console.log("logged inn");
+
+//Question 1
+
+var isLoggedIn = true;
+var myAge = 23;
+var fruit = "Orange";
+
+
+
+
+//Question 2
+
+var fName = "Thomas";
+var lName = "Kildahl";
+var fullName = fName + lName;
+
+console.log(fullName);
+
+
+//Question 3
+
+var animal = "Frog";
+var answer = "The type of frog is "
+console.log(answer + typeof animal);
+
+// Question 4
+
+var orderHasShipped = false;
+
+if(orderHasShipped === true){
+    console.log("The order has Shipped");
+}
+else{
+    console.log("The order did not Shipp");
+}
+
+// Question 5
+
+
+
+
+
+
